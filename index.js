26482305b3c46c9bf32225781ed383c050296d71
Kaveri Menoki <63190649+km-r@users.noreply.github.com>
2020-12-05 10:37:41 +0530

    Drum kit

---
+++ index.js
@@ -0,0 +1,67 @@
+var neededButtons= document.querySelectorAll(".drum").length
+
+for (var i =0;i<neededButtons;i++){
+        document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick(){
+            
+            var buttonInnerHTML=this.innerHTML
+            makeSound(buttonInnerHTML)
+            buttonAnimation(buttonInnerHTML)
+        });
+    }
+        document.addEventListener("keydown",function (event){
+                  
+                  makeSound(event.key)
+                  buttonAnimation(event.key)
+        })
+
+        function makeSound(event){
+        //    var buttonInnerHTML=this.innerHTML
+           switch (event) {
+                  case "a":
+                      var audio= new Audio('sounds/tom-1.mp3');
+                      audio.play();  
+                      break;
+                  case "s":
+                      var audio= new Audio('sounds/tom-2.mp3');
+                      audio.play();  
+                      break;
+                  case "d":
+                      var audio= new Audio('sounds/tom-3.mp3');
+                      audio.play();  
+                      break;
+                  case "f":
+                      var audio= new Audio('sounds/tom-4.mp3');
+                      audio.play();  
+                      break;
+                  case "g":
+                      var audio= new Audio('sounds/crash.mp3');
+                      audio.play();  
+                      break;
+                  case "h":
+                      var audio= new Audio('sounds/kick-bass.mp3');
+                      audio.play();  
+                      break;
+                  case "j":
+                      var audio= new Audio('sounds/snare.mp3');
+                      audio.play();  
+                      break;
+              
+                  default:
+                      alert("wrong key pressed")
+                      break;
+        }
+             
+        
+        }     
+        
+        
+        function buttonAnimation(curremtEvent){
+                    var activeKey=document.querySelector("."+curremtEvent) 
+                    activeKey.classList.add("pressed")
+
+                    setTimeout(function remove() {
+                        activeKey.classList.remove("pressed")
+                        
+                    }, 100);
+        }
+        
